c739ab92025a280d2b4415c72dc47470897bb28d
Otto von Sperling <ottok92@gmail.com>
2019-06-03 16:47:45 -0300

    DONE eval_fibonacci_timecomplexity() with <time.h>

--- fibonacci/fibonacci_main.cpp
+++ fibonacci/fibonacci_main.cpp
@@ -1,4 +1,4 @@
-#include <chrono>
+#include <time.h>
 #include "fibonacci.h"
 
 enum {Matrix=0, Recursive=1, Loop=2, Formula=3};
@@ -6,13 +6,13 @@ enum {Matrix=0, Recursive=1, Loop=2, Formula=3};
 using namespace std;
 using namespace std::chrono;
 
-typedef chrono::duration<long int, std::ratio<1, 1000000> > timedelta_t;
-void eval_fibonacci_timecomplexity(int num_entries, vector< vector<timedelta_t> > *perf_matrix_pt);
+//typedef chrono::duration<long int, std::ratio<1, 1000000> > timedelta_t;
+void eval_fibonacci_timecomplexity(int num_entries, vector< vector<double> > *perf_matrix_pt);
 
 int main(int , const char**)
 {
     vector<int> num_entries {5, 10, 20, 40, 60, 70};
-    vector< vector<timedelta_t> > performance(4);
+    vector< vector<double> > performance(4);
 
 
     for(auto n : num_entries)
@@ -23,7 +23,7 @@ int main(int , const char**)
     for(auto method : performance)
     {
 	for(auto array_element : method)
-		cout << array_element.count() << ", ";
+		cout << array_element << ", ";
 	cout << endl;
     
     }
@@ -31,7 +31,7 @@ int main(int , const char**)
     return 0;
 }
 
-void eval_fibonacci_timecomplexity(int num_entries, vector< vector<timedelta_t> > *perf_mat_pt)
+void eval_fibonacci_timecomplexity(int num_entries, vector< vector<double> > *perf_mat_pt)
 {
 	
     long long n;
@@ -40,35 +40,35 @@ void eval_fibonacci_timecomplexity(int num_entries, vector< vector<timedelta_t> 
     if(num_entries<0 && abs(num_entries)%2==0)   // if N is negative and divisible by 2 then Nth fibonacci must be negative
         sign=-1;
 
-    auto start = high_resolution_clock::now();
-    //n = matrix((long long)num_entries);
-    auto stop = high_resolution_clock::now();
+    clock_t start = clock();
+    n = matrix((long long)num_entries);
+    clock_t stop = clock();
 
-    //perf_mat_pt->at(Matrix).push_back(duration_cast<microseconds>(stop - start));
-    //cout<< "MATRIX -  " << perf_mat_pt->at(Matrix).back().count() << " microseconds for " << num_entries << " entries\n";
+    perf_mat_pt->at(Matrix).push_back((double)(stop - start)/CLOCKS_PER_SEC);
+    cout<< "MATRIX -  " << perf_mat_pt->at(Matrix).back() << " milliseconds for " << num_entries << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
     
-    //start = high_resolution_clock::now(); 
-    //n = recursive((int)num_entries);
-    //stop = high_resolution_clock::now();
+    start = clock(); 
+    n = recursive((int)num_entries);
+    stop = clock();
 
-    //perf_mat_pt->at(Recursive).push_back(duration_cast<microseconds>(stop - start));
-    //cout<< "NAIVE_RECURSION -  " << perf_mat_pt[Recursive][FiveEntries].count() << " microseconds for " << num_entries, << " entries\n";
+    perf_mat_pt->at(Recursive).push_back((double)(stop - start)/CLOCKS_PER_SEC);
+    cout<< "NAIVE_RECURSION -  " << perf_mat_pt->at(Recursive).back() << " milliseconds for " << num_entries << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
     
-    start = high_resolution_clock::now(); 
+    start = clock(); 
     n = loop((int)num_entries);
-    stop = high_resolution_clock::now();
+    stop = clock();
     
-    perf_mat_pt->at(Loop).push_back(duration_cast<microseconds>(stop - start));
-    cout<< "LOOP " << n << " -  " << perf_mat_pt->at(Loop).back().count() << " microseconds for " << num_entries << " entries\n";
+    perf_mat_pt->at(Loop).push_back((double)(stop - start)/CLOCKS_PER_SEC);
+    cout<< "LOOP " << n << " -  " << perf_mat_pt->at(Loop).back() << " milliseconds for " << num_entries << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
     
-    //start = high_resolution_clock::now(); 
-    //n = binets_formula((int)num_entries);
-    //stop = high_resolution_clock::now();
+    start = clock(); 
+    n = binets_formula((int)num_entries);
+    stop = clock();
     
-    //perf_mat_pt->at(Formula).push_back(duration_cast<microseconds>(stop - start));
-    //cout<< "BINETS_FORMULA -  " << perf_mat_pt[Formula][FiveEntries].count() << " microseconds for " << num_entries, << " entries\n";
+    perf_mat_pt->at(Formula).push_back((double)(stop - start)/CLOCKS_PER_SEC);
+    cout<< "BINETS_FORMULA -  " << perf_mat_pt->at(Formula).back() << " milliseconds for " << num_entries << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
 }
