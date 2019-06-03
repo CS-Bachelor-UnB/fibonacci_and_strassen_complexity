1d02cb6bad81fdfaab8ffc906c86d952a68f18fa
Otto von Sperling <ottok92@gmail.com>
2019-06-03 16:21:10 -0300

    DONE eval_fibonacci_timecomplexity: 	+ it is calculating only time in the CPU, which seems to be a problem. 	> changing to time.h

--- fibonacci/fibonacci_main.cpp
+++ fibonacci/fibonacci_main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 using namespace std::chrono;
 
 typedef chrono::duration<long int, std::ratio<1, 1000000> > timedelta_t;
+void eval_fibonacci_timecomplexity(int num_entries, vector< vector<timedelta_t> > *perf_matrix_pt);
 
 int main(int , const char**)
 {
@@ -14,53 +15,60 @@ int main(int , const char**)
     vector< vector<timedelta_t> > performance(4);
 
 
-    for(auto : num_entries)
+    for(auto n : num_entries)
     {
-    	eval_fibonacci_timecomplexity(num_entries, &performance);
+    	eval_fibonacci_timecomplexity(n, &performance);
     }
 
+    for(auto method : performance)
+    {
+	for(auto array_element : method)
+		cout << array_element.count() << ", ";
+	cout << endl;
+    
+    }
     
     return 0;
 }
 
-void eval_fibonacci_timecomplexity(int num_entries, vector< vector<timedelta_t> > *perf_matrix)
+void eval_fibonacci_timecomplexity(int num_entries, vector< vector<timedelta_t> > *perf_mat_pt)
 {
 	
-    long long  N, n;
+    long long n;
     
     int sign=1;
-    if(N<0 && abs(N)%2==0)   // if N is negative and divisible by 2 then Nth fibonacci must be negative
+    if(num_entries<0 && abs(num_entries)%2==0)   // if N is negative and divisible by 2 then Nth fibonacci must be negative
         sign=-1;
 
     auto start = high_resolution_clock::now();
-    n = matrix((long long)N);
+    //n = matrix((long long)num_entries);
     auto stop = high_resolution_clock::now();
 
-    performance[Matrix].push_back(duration_cast<microseconds>(stop - start));
-    cout<< "MATRIX -  " << performance[Matrix][FiveEntries].count() << " milliseconds for " << N << " entries\n";
+    //perf_mat_pt->at(Matrix).push_back(duration_cast<microseconds>(stop - start));
+    //cout<< "MATRIX -  " << perf_mat_pt->at(Matrix).back().count() << " microseconds for " << num_entries << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
     
-    start = high_resolution_clock::now(); 
-    n = recursive((int)N);
-    stop = high_resolution_clock::now();
+    //start = high_resolution_clock::now(); 
+    //n = recursive((int)num_entries);
+    //stop = high_resolution_clock::now();
 
-    performance[Recursive].push_back(duration_cast<microseconds>(stop - start));
-    cout<< "NAIVE_RECURSION -  " << performance[Recursive][FiveEntries].count() << " milliseconds for " << N << " entries\n";
+    //perf_mat_pt->at(Recursive).push_back(duration_cast<microseconds>(stop - start));
+    //cout<< "NAIVE_RECURSION -  " << perf_mat_pt[Recursive][FiveEntries].count() << " microseconds for " << num_entries, << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
     
     start = high_resolution_clock::now(); 
-    n = loop((int)N);
+    n = loop((int)num_entries);
     stop = high_resolution_clock::now();
     
-    performance[Loop].push_back(duration_cast<microseconds>(stop - start));
-    cout<< "LOOP -  " << performance[Loop][FiveEntries].count() << " milliseconds for " << N << " entries\n";
+    perf_mat_pt->at(Loop).push_back(duration_cast<microseconds>(stop - start));
+    cout<< "LOOP " << n << " -  " << perf_mat_pt->at(Loop).back().count() << " microseconds for " << num_entries << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
     
-    start = high_resolution_clock::now(); 
-    n = binets_formula((int)N);
-    stop = high_resolution_clock::now();
+    //start = high_resolution_clock::now(); 
+    //n = binets_formula((int)num_entries);
+    //stop = high_resolution_clock::now();
     
-    performance[Formula].push_back(duration_cast<microseconds>(stop - start));
-    cout<< "BINETS_FORMULA -  " << performance[Formula][FiveEntries].count() << " milliseconds for " << N << " entries\n";
+    //perf_mat_pt->at(Formula).push_back(duration_cast<microseconds>(stop - start));
+    //cout<< "BINETS_FORMULA -  " << perf_mat_pt[Formula][FiveEntries].count() << " microseconds for " << num_entries, << " entries\n";
     // -------------------------------------------------------------------------------------------------------------
 }
