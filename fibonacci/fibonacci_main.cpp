b81fc0ead35fa6f28b6950cafa4bb610dc1a36b0
Otto von Sperling <ottok92@gmail.com>
2019-06-04 11:41:40 -0300

    WIP interpolation: 	> choose between polynomial, lagrange or splice interpolation 	> approximate the O(t(n)) for each algorithm 	> plot (with python perhaps)

--- fibonacci/fibonacci_main.cpp
+++ fibonacci/fibonacci_main.cpp
@@ -1,77 +1,90 @@
 #include <time.h>
+#include <chrono>
 #include "fibonacci.h"
 
 enum {Matrix=0, Recursive=1, Loop=2, Formula=3};
 
 using namespace std;
 
-void eval_fibonacci_timecomplexity(int num_entries, vector< vector<double> > *perf_matrix_pt);
+int eval_fibonacci_timecomplexity(string method, int num_entries, vector< vector<long long> > *perf_matrix_pt);
 
 int main(int , const char**)
 {
-    vector<int> num_entries {5, 10, 20, 40, 60};
-    vector< vector<double> > performance(4);
+	vector<int> num_entries {5, 10, 15, 20, 25, 30,  40, 45};
+	vector< vector<long long> > performance(4);
+	vector<string> algorithms {"matrix", "formula", "loop", "recursive"};
 
-    for(auto n : num_entries)
-    {
-	//interrupt = false;
-    	eval_fibonacci_timecomplexity(n, &performance);
-	cout << endl;
-    }
+	for(auto method : algorithms)
+		for(auto n : num_entries)
+			eval_fibonacci_timecomplexity(method, n, &performance);
+	
+	for(auto array : performance)
+	{
+		for(auto array_element : array)
+			cout << array_element << ", ";
+		cout << endl;
+	}
 
-    for(auto method : performance)
-    {
-	for(auto array_element : method)
-		cout << array_element << ", ";
-	cout << endl;
-    
-    }
-    
-    return 0;
+	return 0;
 }
 
-void eval_fibonacci_timecomplexity(int num_entries, vector< vector<double> > *perf_mat_pt)
+int eval_fibonacci_timecomplexity(string method, int num_entries, vector< vector<long long> > *perf_mat_pt)
 {
 	
-    long long n;
-    
-    int sign=1;
-    if(num_entries<0 && abs(num_entries)%2==0)   // if N is negative and divisible by 2 then Nth fibonacci must be negative
-        sign=-1;
+	long long n;
+	int sign=1;
 
-    clock_t start = clock();
-    n = matrix((long long)num_entries);
-    clock_t stop = clock();
+	if(num_entries<0 && abs(num_entries)%2==0)   // if N is negative and divisible by 2 then Nth fibonacci must be negative
+		sign=-1;
 
-    perf_mat_pt->at(Matrix).push_back((double)(stop - start)/CLOCKS_PER_SEC);
-    cout<< "MATRIX " << n  << " -  " << perf_mat_pt->at(Matrix).back() << " milliseconds for " << num_entries << " entries\n";
-    // -------------------------------------------------------------------------------------------------------------
-    
-    start = clock(); 
-    n = loop((int)num_entries);
-    stop = clock();
-    
-    perf_mat_pt->at(Loop).push_back((double)(stop - start)/CLOCKS_PER_SEC);
-    cout<< "LOOP " << n << " -  " << perf_mat_pt->at(Loop).back() << " milliseconds for " << num_entries << " entries\n";
-    // -------------------------------------------------------------------------------------------------------------
-    
-    start = clock(); 
-    n = binets_formula((int)num_entries);
-    stop = clock();
-    
-    perf_mat_pt->at(Formula).push_back((double)(stop - start)/CLOCKS_PER_SEC);
-    cout<< "BINETS_FORMULA " << n  << " -  " << perf_mat_pt->at(Formula).back() << " milliseconds for " << num_entries << " entries\n";
-    // -------------------------------------------------------------------------------------------------------------
-    
-    if( num_entries < 50  )
-    { 
-	    start = clock(); 
-	    n = recursive((int)num_entries);
-	    stop = clock();
-
-	    perf_mat_pt->at(Recursive).push_back((double)(stop - start)/CLOCKS_PER_SEC);
-	    cout<< "NAIVE_RECURSION " << n  << " -  " << perf_mat_pt->at(Recursive).back() << " milliseconds for " << num_entries << " entries\n";
-    }
-    // -------------------------------------------------------------------------------------------------------------
+	if(method.compare("matrix")==0)
+	{	
+		auto start = std::chrono::high_resolution_clock::now();
+		for( int i=1; i<num_entries; ++i  )
+			n = matrix((long long)i);
+	
+		auto elapsed = std::chrono::high_resolution_clock::now() - start;
+		perf_mat_pt->at(Matrix).push_back(std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
+	    	return(0);
+	}   
+	// -------------------------------------------------------------------------------------------------------------
+	
+	else if(method.compare("loop")==0)
+	{	
+		auto start = std::chrono::high_resolution_clock::now();
+		for( int i=1; i<num_entries; ++i  )
+			n = loop((int)i);
+	
+		auto elapsed = std::chrono::high_resolution_clock::now() - start;
+		perf_mat_pt->at(Loop).push_back(std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
+    		return(0);
+	}   
+	// -------------------------------------------------------------------------------------------------------------
+	
+	else if(method.compare("formula")==0)
+	{	
+		auto start = std::chrono::high_resolution_clock::now();
+		for( int i=1; i<num_entries; ++i  )
+			n = binets_formula((int)i);
+	
+		auto elapsed = std::chrono::high_resolution_clock::now() - start;
+		perf_mat_pt->at(Formula).push_back(std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
+	    	return(0);
+	}   
+	// -------------------------------------------------------------------------------------------------------------
+	
+	else if(method.compare("recursive")==0)
+	{	
+		auto start = std::chrono::high_resolution_clock::now();
+		for( int i=1; i<num_entries; ++i  )
+			n = recursive((int)i);
+	
+		auto elapsed = std::chrono::high_resolution_clock::now() - start;
+		perf_mat_pt->at(Recursive).push_back(std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count());
+    		return(0);
+	}   
+	// -------------------------------------------------------------------------------------------------------------
+	
+	else
+		return(1);
 }
-
